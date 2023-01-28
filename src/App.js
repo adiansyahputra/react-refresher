2ad1d04f8ad45dcebbd55f4cb0095282cfe8d4f8
Adiansyah Putra <adiansyahhputra@gmail.com>
2023-01-28 11:28:50 +0700

    alhamdulillah

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
-import GoalList from './components/GoalList';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
 import './App.css';
 
 const App = () => {
+  const goals = [
+    { id: 'cg1', text: 'Finish the cours' },
+    { id: 'cg2', text: 'Learn all about the course main topic' },
+    { id: 'cg3', text: 'Help other students in the course Q&A' },
+  ];
+
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <GoalList />
+      <NewGoal />
+      <GoalList items={goals} />
     </div>
   );
 };
